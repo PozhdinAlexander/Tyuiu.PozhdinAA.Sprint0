31ed9fde110ea724d0b96881e68cdfb5dab50703
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-09-10 14:13:31 +0500

    Task1

---
+++ Tyuiu.PozhdinAA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PozhdinAA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Александр";
+            int age = 20;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
