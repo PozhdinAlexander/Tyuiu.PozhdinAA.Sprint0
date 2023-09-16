9ff14e50f7161f0f8ec6d2c27792bc684b32e7e5
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-09-16 22:20:59 +0500

    Task6

---
+++ Tyuiu.PozhdinAA.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PozhdinAA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PozhdinAA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92fde81c-e1cb-41a3-a32d-b92072789b38")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PozhdinAA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PozhdinAA.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.PozhdinAA.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
