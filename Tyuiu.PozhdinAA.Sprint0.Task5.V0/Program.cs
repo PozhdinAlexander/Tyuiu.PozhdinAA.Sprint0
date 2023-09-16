208bee8d87eaf0a34bcd4a3dd912ce03b99da28b
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-09-16 21:54:02 +0500

    Task5

---
+++ Tyuiu.PozhdinAA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PozhdinAA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.PozhdinAA.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Division(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
